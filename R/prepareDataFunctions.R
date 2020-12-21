2c3a351ba976509f095aa08fba154eecdc94ecda
villandre <luc.villandre@mail.mcgill.ca>
2020-12-21 18:07:46 -0500

    Still adding functions

--- R/prepareDataFunctions.R
+++ R/prepareDataFunctions.R
@@ -66,9 +66,9 @@ alignAndSaveCanadianData <- function(SARScov2reference, folderForSequences, fold
   currentDateString <- as.character(Sys.Date())
   currentDateStringCorrected <- stringr::str_replace_all(currentDateString, pattern = "-", replacement = "")
   sequencesFilename <- paste(folderWhereToSaveTheResult, "/SARScov2dataAlignedCanada_", currentDateStringCorrected, ".fasta", sep = "")
-  ape::write.dna(SARScov2dataAlignedCanada[ , sitesToKeep] , file = sequencesFilename, format = "fasta", nbcol = 1, colw = 100)
+  ape::write.dna(SARScov2dataAlignedCanada[ , sitesToKeep], file = sequencesFilename, format = "fasta", nbcol = 1, colw = 100)
   cat("Saved sequences in ", sequencesFilename, "\n")
-  sequencesFilename
+  SARScov2dataAlignedCanada[ , sitesToKeep]
 }
 
 extractAndSaveCanadianMetadata <- function(DNAbinObject, folderForMetadata, folderToSaveResult, patternForMetadataFiles = "minimal", patternInSequenceNames = "(?<=(c|C)anada/Qc-).+(?=/2020)", seqNameColumn = "sample", sampleDateColumnName = "sample_date", regionColumnName = "division") {
@@ -108,7 +108,7 @@ extractAndSaveCanadianMetadata <- function(DNAbinObject, folderForMetadata, fold
   filename <- paste(folderToSaveResult, "/canadianMetadata_", currentDateStringCorrected, ".rds", sep = "")
   saveRDS(listToSave, file = filename, compress = TRUE)
   cat("Canadian metadata saved in", filename, "\n")
-  filename
+  listToSave
 }
 
 alignAndSaveGISAIDdata <- function(SARScov2reference, folderForSequences, folderWhereToSaveTheResult, GISAIDfastaFilename, numThreads, extremitiesBoundaries = c(55, 29804), homoplasicSites = c(c(187, 1059, 2094, 3037, 3130, 6990, 8022, 10323, 10741, 11074, 13408, 14786, 19684, 20148, 21137, 24034, 24378, 25563, 26144, 26461, 26681, 28077, 28826, 28854, 29700), c(4050, 13402, 11083, 15324, 21575)), resolutionRequirement = 0.95, numSites = 29903, aligned = FALSE) {
@@ -161,7 +161,7 @@ alignAndSaveGISAIDdata <- function(SARScov2reference, folderForSequences, folder
 
   ape::write.dna(GISAIDdata, file = sequencesFilename, format = "fasta", nbcol = 1, colw = 100)
   cat("Saved GISAID sequences in", sequencesFilename, "\n")
-  sequencesFilename
+  GISAIDdata
 }
 
 extractAndSaveGISAIDmetadata <- function(GISAIDdata, regionsToSampleFrom = c("canada", "australia", "china", "belgium", "denmark", "france",  "england", "finland", "germany", "italy", "portugal", "russia", "spain", "sweden", "turkey", "usa", "wales"), seed, numToSamplePerRegion = 10, folderToSaveResult, fixRegionLabelsList = list(list(replacement = "china", original = c("anhui", "beijing", "chongqing", "fujian", "fuyang", "ganzhou", "guangdong", "guangzhou", "hangzhou", "hefei", "henan", "hong kong", "jian", "jiangsu", "jiangxi", "jingzhou", "jiujiang", "lishui", "nanchang", "pingxiang", "shandong", "shanghai", "shangrao", "shenzhen", "sichuan", "tianmen", "wuhan", "yunnan", "zhejiang")))) {
@@ -194,5 +194,35 @@ extractAndSaveGISAIDmetadata <- function(GISAIDdata, regionsToSampleFrom = c("ca
   filename <- paste(folderToSaveResult, "/GISAIDmetadata_", currentDateStringCorrected, ".rds", sep = "")
   saveRDS(listToSave, file = filename, compress = TRUE)
   cat("GISAID metadata saved in", filename, "\n")
-  filename
+  listToSave
+}
+
+fixCanadianSeqNames <- function(canadianSeqNames) {
+  fixedCanadianSeqNames <- stringr::str_replace(canadianSeqNames, "Canada/Qc-", "")
+  fixedCanadianSeqNames <- stringr::str_replace(fixedCanadianSeqNames, "/2020.*", "")
+  fixedCanadianSeqNames
+}
+
+fixGISAIDseqNames <- function(GISAIDseqNames) {
+  extractedNames <- stringr::str_extract(GISAIDseqNames, "(?<=hCoV-19/).+")
+  extractedNames <- stringr::str_replace_all(extractedNames, "_", "")
+  extractedNames <- stringr::str_replace_all(extractedNames, " ", "")
+  extractedNames <- stringr::str_replace_all(extractedNames, "\\|", "_")
+  stringr::str_replace_all(extractedNames, "/", "-")
+}
+
+identifyNonOutlierInternationalSeqs <- function(SARScov2reference, canadianDNAbinData, internationalDNAbinData, canadianIndices, internationalIndices, extremitiesBoundaries = c(55, 29804), homoplasicSites = c(c(187, 1059, 2094, 3037, 3130, 6990, 8022, 10323, 10741, 11074, 13408, 14786, 19684, 20148, 21137, 24034, 24378, 25563, 26144, 26461, 26681, 28077, 28826, 28854, 29700), c(4050, 13402, 11083, 15324, 21575)), excessDistThreshold = 2) {
+  SARScov2reference <- as.matrix(SARScov2reference)[ , -c(1:extremitiesBoundaries[[1]], extremitiesBoundaries[[2]]:29903, homoplasicSites)]
+  distToRef <- lapply(
+    list(Canada = as.matrix(canadianDNAbinData)[canadianIndices, ], International = as.matrix(internationalDNAbinData)[internationalIndices, ]), function(dataset) {
+      distToRef <- sapply(1:nrow(dataset), function(seqIndex) {
+        seqPair <- rbind(dataset[seqIndex, ], SARScov2reference[1, ])
+        ape::dist.dna(seqPair, pairwise.deletion = TRUE, as.matrix = TRUE)[1, 2]
+      })
+    })
+  print(lapply(distToRef, summary))
+  cat("Maximum distance for Canadian sequences:", max(distToRef$Canada), "reached for sequence", names(canadianDNAbinData)[which.max(distToRef$Canada)], "\n")
+  cat("Maximum distance for international sequences:", max(distToRef$International), "reached for sequence", names(internationalDNAbinData)[which.max(distToRef$International)], "\n")
+  maxAllowedDistance <- max(distToRef$Canada) * excessDistThreshold
+  internationalIndices[distToRef$International <= maxAllowedDistance]
 }
